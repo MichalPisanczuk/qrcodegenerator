39657518469222460fdc10cb369599a471cc5b29
Michal Pisanczuk <michal@pisanczuk.com>
2022-08-17 14:50:32 +0200

    Add error correction level with input range

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {
   NgxQrcodeElementTypes,
   NgxQrcodeErrorCorrectionLevels,
   QrcodeComponent,
 } from '@techiediaries/ngx-qrcode';
- import { FileSaverService } from 'ngx-filesaver';
+import { FileSaverService } from 'ngx-filesaver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   public elementType = NgxQrcodeElementTypes.URL;
   public correctionLevel = NgxQrcodeErrorCorrectionLevels.LOW;
   public value = 'pisanczuk.com';
@@ -19,31 +19,43 @@ export class AppComponent implements AfterViewInit {
   public qrcolor = 'ffffff';
   public bgcolor = '000000';
   public scale = 3;
-  public level: any;
+  public selectedLevel = 2;
 
   @ViewChild('imgqr') qrImg!: ElementRef;
   @ViewChild(QrcodeComponent) qrComp!: QrcodeComponent;
 
-  constructor(private _fileSaverService:FileSaverService) {}
-
-  // switch(this.level) {
-  //   case 1: this.correctionLevel = NgxQrcodeErrorCorrectionLevels.LOW;
-  //   case 2: this.correctionLevel = NgxQrcodeErrorCorrectionLevels.MEDIUM;
-  //   case 3: this.correctionLevel = NgxQrcodeErrorCorrectionLevels.QUARTILE;
-  //   case 4: this.correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
-  // }
-
-  ngAfterViewInit() {
-    console.log(this.qrImg);
-
-
+  constructor(private _fileSaverService: FileSaverService) {
+    
   }
+
+ getNewLevel() {
+  switch (this.selectedLevel) {
+    case 1:
+      this.correctionLevel = NgxQrcodeErrorCorrectionLevels.LOW;
+      console.log(this.correctionLevel);
+      break;
+    case 2:
+      this.correctionLevel = NgxQrcodeErrorCorrectionLevels.MEDIUM;
+      console.log(this.correctionLevel);
+      break;
+    case 3:
+      this.correctionLevel = NgxQrcodeErrorCorrectionLevels.QUARTILE;
+      console.log(this.correctionLevel);
+      break;
+    case 4:
+      this.correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+      console.log(this.correctionLevel);
+      break;
+  }
+  return this.correctionLevel;
+ }
+
   saveQrCode() {
     this.qrComp['toDataURL']().then((data: string) => {
       this._fileSaverService.save(this._dataURItoBlob(data), 'qr.png');
     });
   }
-  _dataURItoBlob(dataURI:string) {
+  _dataURItoBlob(dataURI: string) {
     // convert base64/URLEncoded data component to raw binary data held in a string
 
     var byteString;
